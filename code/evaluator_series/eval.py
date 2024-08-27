2c081ffadc7b882e58487ef2342ef5c1a15c3b07
Jelly Lee <liguodongiot@163.com>
2024-08-27 20:49:37 +0800

    新增vllm部署qwen1.5测评

--- code/evaluator_series/eval.py
+++ code/evaluator_series/eval.py
@@ -6,6 +6,8 @@ from evaluators.chatgpt import ChatGPT_Evaluator
 from evaluators.moss import Moss_Evaluator
 from evaluators.chatglm import ChatGLM_Evaluator
 from evaluators.minimax import MiniMax_Evaluator
+from evaluators.qwen_1_5 import Qwen_1_5_Evaluator
+
 
 import time
 choices = ["A", "B", "C", "D"]
@@ -43,25 +45,39 @@ def main(args):
             api_key=args.minimax_key,
             model_name=args.model_name
         )
+    elif "qwen1.5" in args.model_name:
+        evaluator=Qwen_1_5_Evaluator(
+            choices=choices,
+            k=args.ntrain,
+            model_name=args.model_name
+        )
     else:
         print("Unknown model name")
         return -1
 
     subject_name=args.subject
+
     if not os.path.exists(r"logs"):
         os.mkdir(r"logs")
+
     run_date=time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
+
     save_result_dir=os.path.join(r"logs",f"{args.model_name}_{run_date}")
+
     os.mkdir(save_result_dir)
-    print(subject_name)
+    print("------", subject_name)
+
     val_file_path=os.path.join('data/val',f'{subject_name}_val.csv')
     val_df=pd.read_csv(val_file_path)
+
+
     if args.few_shot:
         dev_file_path=os.path.join('data/dev',f'{subject_name}_dev.csv')
         dev_df=pd.read_csv(dev_file_path)
         correct_ratio = evaluator.eval_subject(subject_name, val_df, dev_df, few_shot=args.few_shot,save_result_dir=save_result_dir,cot=args.cot)
     else:
-        correct_ratio = evaluator.eval_subject(subject_name, val_df, few_shot=args.few_shot,save_result_dir=save_result_dir)
+        correct_ratio = evaluator.eval_subject(subject_name, val_df, few_shot=args.few_shot, save_result_dir=save_result_dir)
+
     print("Acc:",correct_ratio)
 
 
@@ -75,6 +91,6 @@ if __name__ == "__main__":
     parser.add_argument("--model_name",type=str)
     parser.add_argument("--cot",action="store_true")
     parser.add_argument("--subject","-s",type=str,default="operating_system")
-    parser.add_argument("--cuda_device", type=str)    
+    parser.add_argument("--cuda_device", type=str)
     args = parser.parse_args()
     main(args)
---
+++ code/evaluator_series/evaluators/qwen_1_5.py
@@ -0,0 +1,207 @@
+import os
+import re
+from tqdm import tqdm
+import json
+import torch
+from transformers import AutoTokenizer, AutoModel
+from evaluators.evaluator import Evaluator
+from time import sleep
+import requests
+
+url = "http://10.112.2.145:9009/v1/chat/completions"
+
+
+
+class Qwen_1_5_Evaluator(Evaluator):
+    def __init__(self, choices, k, model_name):
+        super(Qwen_1_5_Evaluator, self).__init__(choices, model_name, k)
+        #self.tokenizer = AutoTokenizer.from_pretrained("/workspace/models/Qwen1.5-7B-Chat", trust_remote_code=True)
+        #self.model = AutoModel.from_pretrained("/workspace/models/Qwen1.5-7B-Chat",  trust_remote_code=True).half().to(device)
+
+    def format_example(self, line, include_answer=True, cot=False):
+
+        example=line['question']
+
+        for choice in self.choices:
+            example+=f'\n{choice}. {line[f"{choice}"]}'
+
+        example+='\n答案：'
+
+        if include_answer:
+            if cot:
+                ans=line["answer"]
+                content="让我们一步一步思考，\n"+line["explanation"]+f"\n所以答案是{ans}。"
+                return [
+                    {"role":"user","content":example},
+                    {"role":"assistant","content":content}
+                ]
+            else:
+                return [
+                    {"role":"user","content":example},
+                    {"role":"assistant","content":line["answer"]}
+                ]
+        else:
+            # False
+            return [
+                {"role":"user","content":example},
+            ]
+
+    def generate_few_shot_prompt(self, subject, dev_df, cot=False):
+        prompt=[
+            {
+                "role":"system",
+                "content":f"你是一个中文人工智能助手，以下是中国关于{subject}考试的单项选择题，请选出其中的正确答案。"
+            }
+        ]
+        k=self.k
+        if self.k==-1:
+            k=dev_df.shape[0]
+        for i in range(k):
+            tmp=self.format_example(dev_df.iloc[i,:],include_answer=True,cot=cot)
+            if i==0:
+                tmp[0]["content"]=f"以下是中国关于{subject}考试的单项选择题，请选出其中的正确答案。\n\n"+tmp[0]["content"]
+            prompt+=tmp
+        return prompt
+
+    def eval_subject(self, subject_name, test_df, dev_df=None, few_shot=False, save_result_dir=None,cot=False):
+        correct_num = 0
+        if save_result_dir:
+            result = []
+            score=[]
+
+        if few_shot:
+            few_shot_prompt = self.generate_few_shot_prompt(subject_name, dev_df,cot=cot)
+        else:
+            few_shot_prompt=[
+                {
+                    "role":"system",
+                    "content":f"你是一个中文人工智能助手，以下是中国关于{subject_name}考试的单项选择题，请选出其中的正确答案。"
+                }
+            ]
+
+
+        answers = list(test_df['answer'])
+
+        for row_index, row in tqdm(test_df.iterrows(), total=len(test_df)):
+            question = self.format_example(row, include_answer=False)
+            full_prompt = few_shot_prompt + question
+
+            if not few_shot:
+                full_prompt[-1]["content"]=f"以下是中国关于{subject_name}考试的单项选择题，请选出其中的正确答案。\n\n"+full_prompt[-1]["content"]
+
+            print(str(full_prompt))
+
+            response=None
+            timeout_counter=0
+            while response is None and timeout_counter<=30:
+
+                try:
+                    payload = {
+                        "model": "qwen1.5",
+                        "messages": full_prompt,
+                        "max_tokens": 256,
+                        "top_p": 0.95,
+                        "seed": 100,
+                        "temperature": 0.8,
+                        "stream": False
+                    }
+
+                    headers = {"content-type": "application/json"}
+                    response = requests.request("POST", url, json=payload, headers=headers)
+                    print(response.text)
+
+                except Exception as msg:
+                    if "timeout=600" in str(msg):
+                        timeout_counter+=1
+                    print(msg)
+                    sleep(5)
+                    continue
+
+            if response==None:
+                response_str=""
+            else:
+                response_json = json.loads(response.text)
+                response_str = response_json['choices'][0]['message']['content']
+
+            print(response_str)
+            print(f"\n--------------{row_index}----------------\n")
+
+            if cot:
+                ans_list=re.findall(r"答案是(.+?)。",response_str)
+                if len(ans_list)==0:
+                    ans_list=re.findall(r"答案为(.+?)。",response_str)
+                if len(ans_list)==0:
+                    ans_list=re.findall(r"选项(.+?)是正确的。",response_str)
+
+                if len(ans_list)==0:
+                    correct=0
+                else:
+                    if self.exact_match(ans_list[-1],row["answer"]):
+                        correct_num+=1
+                        correct=1
+                    else:
+                        correct=0
+            else:
+                response_str=response_str.strip()
+                if few_shot:
+                    if len(response_str)>0:
+                        if self.exact_match(response_str,row["answer"]):
+                            correct_num+=1
+                            correct=1
+                        else:
+                            correct=0
+                    else:
+                        correct=0
+                else:
+                    if len(response_str)>0:
+                        ans_list=self.extract_ans(response_str)
+                        print("正确答案：", answers[row_index])
+                        print("实际答案：", str(ans_list))
+                        print("\n-------------------------\n")
+                        if len(ans_list)>0 and (ans_list[-1]==row["answer"]):
+                            correct_num+=1
+                            correct=1
+                        else:
+                            correct=0
+                    else:
+                        correct=0
+            if save_result_dir:
+                result.append(response_str)
+                score.append(correct)
+        correct_ratio = 100*correct_num/len(answers)
+
+        if save_result_dir:
+            test_df['model_output']=result
+            test_df["correctness"]=score
+            test_df.to_csv(os.path.join(save_result_dir, f'{subject_name}_val.csv'),encoding="utf-8",index=False)
+        return correct_ratio
+
+    def extract_ans(self,response_str):
+        pattern=[
+            r"答案是\s?选?项?\s?([A-D])",
+            r"答案为\s?选?项?\s?([A-D])",
+            r"答案应为\s?选?项?\s?([A-D])",
+            r"答案选\s?选?项?\s?([A-D])",
+            r"答案是:\s?选?项?\s?([A-D])",
+            r"答案应该是:\s?选?项?\s?([A-D])",
+            r"正确的一项是\s?([A-D])",
+            r"答案为:\s?选?项?\s?([A-D])",
+            r"答案应为:\s?选?项?\s?([A-D])",
+            r"答案:\s?选?项?\s?([A-D])",
+            r"答案是：\s?选?项?\s?([A-D])",
+            r"答案应该是：\s?选?项?\s?([A-D])",
+            r"答案为：\s?选?项?\s?([A-D])",
+            r"答案应为：\s?选?项?\s?([A-D])",
+            r"答案：\s?选?项?\s?([A-D])",
+            r"选([A-D])",
+            r"选项([A-D])",
+        ]
+        ans_list=[]
+        if response_str[0] in ["A",'B','C','D']:
+            ans_list.append(response_str[0])
+        for p in pattern:
+            if len(ans_list)==0:
+                ans_list=re.findall(p,response_str)
+            else:
+                break
+        return ans_list
